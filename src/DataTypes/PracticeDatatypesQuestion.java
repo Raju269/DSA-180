41384b3a6adbf3992fdeb7169c442a2ba8ded2ca
Rajukumar <rajuk631149@gmail.com>
2026-04-13 19:17:37 +0530

    complete datatypes is done

---
+++ src/DataTypes/PracticeDatatypesQuestion.java
@@ -0,0 +1,22 @@
+package DataTypes;
+
+public class PracticeDatatypesQuestion {
+    public static void main(String[] args) {
+
+//        Number of contests attended
+//        Average problem solving time
+//        Whether premium member or not
+//        Grade in mock test
+        int contestsAttend = 23;
+        System.out.println(contestsAttend);
+
+        short probemSolving = 12;
+        System.out.println(probemSolving);
+
+        boolean isPremiumMember = false;
+        System.out.println(isPremiumMember);
+
+        char mockTest = 'B';
+        System.out.println(mockTest);
+    }
+}
---
+++ src/DataTypes/dataTypesJava.java
@@ -0,0 +1,33 @@
+package DataTypes;
+
+public class dataTypesJava {
+    public static void main(String[] args) {
+//        Primitive Data Types in Java
+        boolean isLoggined = true;
+        System.out.println(isLoggined);
+        byte moduleLevel = 34;
+        System.out.println(moduleLevel);
+        short contestScore = 1940;
+        System.out.println(contestScore);
+        int marks =23;
+        System.out.println(marks);
+        long distanceOfSun = 343325454343l;
+        System.out.println(distanceOfSun);
+        double meansValue = 234.434d;
+        System.out.println(meansValue);
+        char isSymbols = 'A';
+        System.out.println(isSymbols);
+
+//        Non Primitive Data Types in Java
+//        Primitive vs Reference Types
+//        String
+//        Arrays
+//        Classes
+//        Objects
+        String isMyName = "Raju kumar ";
+        System.out.println(isMyName);
+        String trackName = "DSA Mastery Track";
+        System.out.println(trackName);
+
+    }
+}

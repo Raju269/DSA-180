6ab1c9d87895693e362ffae6bc153798d7958a6b
Rajukumar <rajuk631149@gmail.com>
2026-05-07 19:56:01 +0530

    pattern printing question 3  is done

--- src/Pattern_Printing_1/BasicPrinting.java
+++ src/Pattern_Printing_1/BasicPrinting.java
@@ -1,4 +1,31 @@
 package Pattern_Printing_1;
 
+import java.util.Scanner;
+
 public class BasicPrinting {
+    public static  int squarePrinting(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        return  0;
+    }
+    public static void main(String[] args) {
+        System.out.println("starting with pattern printing question ok \n ");
+//        square pattern printing \]
+        Scanner src = new Scanner(System.in);
+        System.out.println("Enter the value of n is : ");
+        int n = src.nextInt();
+        for(int i=1;i<=n; i++){
+            for(int j=1;j<=n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("this function call square printing ");
+        squarePrinting(n);
+
+    }
 }

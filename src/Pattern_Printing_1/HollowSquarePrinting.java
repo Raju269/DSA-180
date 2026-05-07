dfe53decaf6cab637eec0d619c50dd10ff17c2c6
Rajukumar <rajuk631149@gmail.com>
2026-05-07 19:56:39 +0530

    basic question is done

--- src/Pattern_Printing_1/HollowSquarePrinting.java
+++ src/Pattern_Printing_1/HollowSquarePrinting.java
@@ -1,4 +1,41 @@
 package Pattern_Printing_1;
 
+import java.util.Scanner;
+
 public class HollowSquarePrinting {
+    public static  int hollowSquarePrinting(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==n-1 || j==n-1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        System.out.println("Enter the value of n is : ");
+        int n = src.nextInt();
+        System.out.println("Enter the value of m is : ");
+        int m = src.nextInt();
+//        Hollow square
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("printing function call ");
+        hollowSquarePrinting(n);
+    }
 }

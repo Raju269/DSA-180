2092420baa787685e8e6c41c3e22ec80f9ced268
Rajukumar <rajuk631149@gmail.com>
2026-05-07 19:55:40 +0530

    pattern printing question 2  is done

--- src/Pattern_Printing_1/AlphabetSquarePrinting.java
+++ src/Pattern_Printing_1/AlphabetSquarePrinting.java
@@ -1,4 +1,21 @@
 package Pattern_Printing_1;
 
+import java.util.Scanner;
+
 public class AlphabetSquarePrinting {
+    public static int alphabetPrinting(int n){
+        for(char i='a';i<='a'+n;i++){
+            for(char j='a';j<='a'+n;j++){
+                System.out.print((char) j+" ");
+            }
+            System.out.println();
+        }
+        return  0;
+    }
+    public static void main(String[] args) {
+      Scanner src = new Scanner(System.in);
+        System.out.println("Enter the value  of n is : ");
+        int n = src.nextInt();
+        alphabetPrinting(n);
+    }
 }
